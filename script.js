813bdeb165524d07d600a11fec2c115b0faab670
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-06-19 21:58:03 -0300

    Add amounts of attempts to guess

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var numero = Math.floor(Math.random() * 10);
+var tentativas = 0;
+
+// Resposta do valor para testar
 document.getElementById("dica").innerHTML = numero;
 
 function verificarValor(){
 
 	var num = document.getElementById("num1").value;
+	tentativas += 1;
+
+	document.getElementById("qtdeTentativas").innerHTML = "Quantidade de Tentativas: " + tentativas;
 
 	if(num == numero){
-		window.alert("Parabéns você descobriu!");
+		document.getElementById("dica").innerHTML = "Parabéns você acertou!";
 	}
 	else{
 		verificaMenor(num);
@@ -15,7 +21,7 @@ function verificarValor(){
 
 function verificaMenor(valor){
 	if(valor < numero){
-		window.alert("Você errou, mas vou te dar uma dica: O número que estou pensando é maior do que você digitou!");
+		document.getElementById("dica").innerHTML = "Você errou, mas vou te dar uma dica: O número que estou pensando é maior do que você digitou!";
 	}
 	else{
 		verificaMaior(valor);
@@ -24,6 +30,17 @@ function verificaMenor(valor){
 
 function verificaMaior(valor){
 	if(valor > numero){
-		window.alert("Você errou, mas vou te dar uma dica: O número que estou pensando é menor do que você digitou!");
+		document.getElementById("dica").innerHTML = "Você errou, mas vou te dar uma dica: O número que estou pensando é menor do que você digitou!";
+	}
+}
+
+function verificaQtdeTentativas(){
+	if(tentativas == 4){
+		document.getElementById("dica").innerHTML = "Você perdeu!! Tente Novamente";
+		numero = Math.floor(Math.random() * 10);
+		tentativas = 0;
+	}
+	else{
+		verificarValor();
 	}
 }
